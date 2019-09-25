f416863a09a5d8665874edacf3ea36783e53d77d
Kayra Czerniewicz <kayracz@gmail.com>
2019-09-24 20:31:27 -0400

    added readme

--- lib/KayViewTool.rb
+++ lib/KayViewTool.rb
@@ -2,10 +2,5 @@ require "KayViewTool/version"
 require "KayViewTool/renderer"
 
 module KayViewTool
-  class Error < StandardError; end
-  # Your code goes here...
-end
-
-module KayViewTool
   # Your code goes here...
 end
