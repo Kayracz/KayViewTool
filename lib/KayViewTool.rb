926cd5306a7905661081921751e902ac21916531
Kayra Czerniewicz <kayracz@gmail.com>
2019-09-24 20:23:15 -0400

    Dev tool initial commit

---
+++ lib/KayViewTool.rb
@@ -0,0 +1,11 @@
+require "KayViewTool/version"
+require "KayViewTool/renderer"
+
+module KayViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+module KayViewTool
+  # Your code goes here...
+end
---
+++ lib/KayViewTool/version.rb
@@ -0,0 +1,3 @@
+module KayViewTool
+  VERSION = "0.1.0"
+end
